0a1e6558ee791128023ae9f8187256a2bec9beef
fahmifhusin <fahmi13farhan@gmail.com>
2023-11-06 11:55:56 +0700

    Changes : - integrasi api Notes : - harus ada amplify auth sama datastore angger. - referensi dari sini https://github.com/mapbox/mapbox-maps-flutter

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,14 @@
+export type AmplifyDependentResourcesAttributes = {
+  "storage": {
+    "dynamomapbox": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
+    }
+  }
+}
